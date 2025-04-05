c9d451a742c25567002f2884b8de590321519aba
DaniDeveloper11 <151704893+DaniDeveloper11@users.noreply.github.com>
2025-04-05 13:04:27 -0600

    add: morgan and some feactures

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userRoutes = require('./routes/user.routes');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
